2aab4f7adb468b321889e7d4f2d9b5635233d5a0
Gabi Gutu <gabi.gutu@gmail.com>
2021-08-09 13:34:01 +0300

    Added Zipkin

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+X
+(src/main/java/com/db/AP2Application.java,8/e/8eac6de50d1f59a92b80ea22ba47eeb5287af020
+S
+#src/main/java/com/db/POJO/User.java,4/d/4de630d874908e2d7b83a7d96ceebfa095256a23
+c
+3src/main/java/com/db/controller/UserController.java,d/a/da3624513478241043ef4a3d47642f40009a1b33
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
--- src/main/java/com/db/AP2Application.java
+++ src/main/java/com/db/AP2Application.java
@@ -1,11 +1,18 @@
 package com.db;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class AP2Application {
+
+//    @Autowired
+//    private ApplicationContext applicationContext;
+
     public static void main(String[] args) {
         SpringApplication.run(AP2Application.class, args);
+
     }
 }
